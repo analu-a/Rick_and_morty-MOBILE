1f12a0cf179ea3ad02c20c12e40116fc22366545
ANA LUIZA AZEVEDO MORAIS <23111713@dsn127m1247443.cfp127aluno.local>
2024-09-03 16:33:48 -0300

    teste

---
+++ app/src/main/java/br/senai/sp/jandira/rickandmorty/service/CharacterService.kt
@@ -0,0 +1,16 @@
+package br.senai.sp.jandira.rickandmorty.service
+
+import br.senai.sp.jandira.rickandmorty.model.Character
+import br.senai.sp.jandira.rickandmorty.model.Result
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CharacterService {
+
+    @GET("character")
+    fun getAllCharacters(): Call<Result>
+
+    @GET("character/{id}")
+    fun getCharacterById(@Path("id") id: Int): Call<Character>
+}
